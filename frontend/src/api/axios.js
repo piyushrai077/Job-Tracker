0e95c11c3a777f44485d2fec9fe098332a3e73cd
piyushrai077 <raipiyush422@gmail.com>
2026-07-04 10:57:48 +0530

    Update API URL to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api' // backend server ka address
+  baseURL: 'https://job-tracker-api-vcxa.onrender.com/api'
 });
 
-// har request ke saath automatically token bhejta hai (agar login hai toh)
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
